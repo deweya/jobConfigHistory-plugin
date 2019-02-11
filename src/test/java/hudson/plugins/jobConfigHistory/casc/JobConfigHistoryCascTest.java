32167122bbb356811caa998ae4a84385adf3c91f
Austin Dewey <adewey@redhat.com>
2019-02-11 12:56:27 -0500

    Adding unit test

--- src/test/java/hudson/plugins/jobConfigHistory/casc/JobConfigHistoryCascTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/casc/JobConfigHistoryCascTest.java
@@ -2,8 +2,10 @@ package hudson.plugins.jobConfigHistory.casc;
 
 import org.junit.Rule;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import hudson.plugins.jobConfigHistory.JobConfigHistory;
 import io.jenkins.plugins.casc.ConfigurationAsCode;
 import io.jenkins.plugins.casc.yaml.YamlSource;
 
@@ -15,11 +17,7 @@ public class JobConfigHistoryCascTest {
     @Test
     public void configure_job_config_history() throws Exception {
         ConfigurationAsCode.get().configureWith(new YamlSource(getClass().getResourceAsStream("jobConfigHistoryCasc.yaml"), YamlSource.READ_FROM_INPUTSTREAM));
-    }
-    
-    @Test
-    public void configure_system_credentials() throws Exception {
-        ConfigurationAsCode.get().configureWith(new YamlSource(getClass().getResourceAsStream("SystemCredentialsTest.yaml"), YamlSource.READ_FROM_INPUTSTREAM));
-        
+        assertEquals(JobConfigHistory.getInstance().getMaxHistoryEntries(), "100");
+        assertEquals(JobConfigHistory.getInstance().getMaxEntriesPerPage(), "20");
     }
 }
